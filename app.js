477dfc537c2bcbf7b7d3fce4486d9c4e7fcc4325
unknown <mc0902168@otc.edu>
2021-05-18 13:27:58 -0500

    api call using axios and vue if else added

--- app.js
+++ app.js
@@ -2,20 +2,24 @@
 const app = new Vue({
     el: "#app", //Gets at the element with the id of app
     data: {
-        curTemp: "testing",
+        temp: "",
         curConditions: "",
         curCity: "",
         zipCode: 65738,
-        weather:[
-            {curTemp:"66", curConditions:"Partly Cloudy", curcity:"Republic"}
+        apiParam: "imperial",
+        imp: "° f",
+        wthrs:[
+            {temp:"66", curConditions:"Partly Cloudy", curcity:"Republic"}
         ]
     },
     methods:{
         curWeather() {
-            weather = "api.openweathermap.org/data/2.5/weather?zip=65738 US&appid=6df605a29ea5d833a7dbfa5767e8d5b0"
-            axios.get(weather)
+            apiString = "https://api.openweathermap.org/data/2.5/weather?zip=65738,us&units="+ this.apiParam + "&appid=6df605a29ea5d833a7dbfa5767e8d5b0"
+            axios.get(apiString)
             .then(function (e){
-                console.log(e)
+                console.log(e.data.main) //to see my results in the console for reference
+                app.wthrs = e.data.main
+                
             })
         }
 
