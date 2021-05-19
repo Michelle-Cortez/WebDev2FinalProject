8163d0f6f91c9585909e3ed80c56c11fac2c2630
unknown <mc0902168@otc.edu>
2021-05-19 04:26:53 -0500

    added input for zip code and option for metric or imperial. added bootstrap card for output and container for input. added background image

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = new Vue({
         apiParam: "imperial",
         imp: "° f",
         met: "° c",
+       
         wthrs:[
             {temp:"66", curConditions:"Partly Cloudy", curCity:"Republic"}
         ],
@@ -16,13 +17,14 @@ const app = new Vue({
     },
     methods:{
         curWeather() {
-            apiString = "https://api.openweathermap.org/data/2.5/weather?zip=65738,us&units="+ this.apiParam + "&appid=6df605a29ea5d833a7dbfa5767e8d5b0"
+            apiString = "https://api.openweathermap.org/data/2.5/weather?zip="+ this.zipCode +",us&units="+ this.apiParam + "&appid=6df605a29ea5d833a7dbfa5767e8d5b0"
             axios.get(apiString)
             .then(function (e){
-                console.log(e.data.weather[0].description) //to see my results in the console for reference
+                console.log(e.data) //to see my results in the console for reference
                 
                 app.wthrs = e.data.main
                 app.curCity = e.data.name
+                app.conds = e.data.weather[0]
 
             })
         }
